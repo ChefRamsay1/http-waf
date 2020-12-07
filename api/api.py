6ed070f2e090a3e04aa65da7417dc45d52aeaaee
Gordon <gwren13@gmail.com>
2020-12-07 16:14:30 -0500

    refined api comments, added test_api_perf

--- api/api.py
+++ api/api.py
@@ -3,9 +3,6 @@ from flask_api import status
 import sys
 import json
 
-app = Flask(__name__)
-app.config["DEBUG"] = True
-
 # A simple HTTP service that will accept requests
 # and return either 200 OK or 403 Forbidden.
 
@@ -16,28 +13,33 @@ app.config["DEBUG"] = True
 #   { "is_malicious": true } and { "hidden": { "is_malicious": true } } => 403 Forbidden
 #   { "is_malicious": false } and { "data": null } => 200 OK
 
-success = {"status": "success", "code": 200, "message": ""}  # success response body
-forbidden = {"status": "error", "code": 403, "message": "is_malicious found"}  # forbidden response body
-IS_MALICIOUS = "is_malicious"
+app = Flask(__name__)
+app.config["DEBUG"] = True
 
-home_page_html = """<h1>Simple Web Application Firewall</h1>
-                    <p>A simple HTTP service that will accept requests and return either 200 OK or 403 Forbidden</p>
-                    <p>To scan a request for "is_malicious" send POST request to /api/handle-request</p>"""
+success = {"status": "success", "code": 200,
+           "message": ""}  # success response body
+forbidden = {"status": "error", "code": 403,
+             "message": "is_malicious found"}  # forbidden response body
+IS_MALICIOUS = "is_malicious"
 
 
 @app.route('/', methods=['GET'])
 def home():
     return """<h1>Simple Web Application Firewall</h1>
                     <p>A simple HTTP service that will accept requests and return either 200 OK or 403 Forbidden</p>
-                    <p>To scan a request for "is_malicious" send POST request to /api/handle-request</p>
+                    <p>To scan a request for "is_malicious" send POST request to .../api/handle-request</p>
                     <p> ex: localhost:5000/api/handle-request with request body { "is_malicious": true } </p>"""
 
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
-# returns 403 Forbidden if request body contains { "is_malicious": true } anywhere
-# otherwise returns 200 Ok
+# Function to handle http requests.
+# if request body contains { "is_malicious": true }
+#   returns 403 Forbidden 
+# else returns 200 Ok
+#
+# Function/Assignment acts as a firewall so should handle all methods with a body
 @app.route('/api/handle-request', methods=['HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'])
 def handleRequest():
     try:  # try to deserialize request body
@@ -45,24 +47,24 @@ def handleRequest():
     except ValueError:  # if no text or bad format
         return success  # return 200 OK as there is no { "is_malicious": true }
 
-
-    # iterate through the object recursively to get all ((key, key,..), value) in nested object
+    # iterate through the object recursively to get all (key, value) pairs in nested object
     for key, value in traverse_object(req_data):
-        
+
         # if the key == "is_malicious" and the value is True
         if key == IS_MALICIOUS and value == True:
-            return forbidden, status.HTTP_403_FORBIDDEN
+            return forbidden, status.HTTP_403_FORBIDDEN # return informative forbidden object with status code 403 FORBIDDEN
 
-    return success, status.HTTP_200_OK
+    return success, status.HTTP_200_OK # return informative success object with status code 200 OK
 
 
-# a generator function to return key value pairs with a depth first search order
+# a generator function to find key value pairs with a depth first search order.
+# Generator computes values as needed rather than computing them all at once and returning as a list.
 # input: Object
-# output: list of keys, value pairs
-# keys - list of keys navigating the object to the value
+# output: list of key, value pairs
+# key - a key or index in object mapped to a value
 # value - value in the object that is not a dictionary, list, or tuple
 # example:
-# input: 
+# input:
 # {
 #     "language" : "Python",
 #     "framework" : "Flask",
@@ -74,25 +76,25 @@ def handleRequest():
 #     "examples" : [{ "is_malicious": {"is_malicious": "false"}}, "form", "json"],
 #     "boolean_test" : true
 # }
-# output: 
-# (('language',), 'Python')
-# (('framework',), 'Flask')
-# (('website',), 'Scotch')
-# (('version_info', 'python'), 3.4)
-# (('version_info', 'flask'), 0.12)
-# (('examples', 0, 'is_malicious', 'is_malicious'), 'false')
-# (('examples', 1), 'form')
-# (('examples', 2), 'json')
-# (('boolean_test',), True)
+# execution output:
+# ('language', 'Python')
+# ('framework', 'Flask')
+# ('website', 'Scotch')
+# ('python', 3.4)
+# ('flask', 0.12)
+# ('is_malicious', 'false')
+# (1, 'form')
+# (2, 'json')
+# ('boolean_test', True)
 def traverse_object(obj, parent_key=None):
     if isinstance(obj, dict):  # if the object is a dictionary
         for key, value in obj.items():  # for each key value pair in the dictionary
-            yield from traverse_object(value, key)
-    elif isinstance(obj, list):
-        for idx, item in enumerate(obj):
-            yield from traverse_object(item, idx)
+            yield from traverse_object(value, key) # keep yielding key,value pairs as needed recursively
+    elif isinstance(obj, list): # if the object is a list
+        for idx, item in enumerate(obj): # for each index, item in the list
+            yield from traverse_object(item, idx) # recursively traverse each object in list
     else:
-        yield parent_key, obj
+        yield parent_key, obj # else(basecase), the obj is not a dict or list just yield the simple parent_key, object pair
 
 
-app.run()
+app.run() # start the app
